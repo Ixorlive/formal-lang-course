172a688b5e87244816c2c73c086de2ad9423e3bf
ixorlive <igorshakirov43@gmail.com>
2023-04-16 00:56:51 +0300

    task 8 completed

--- project/graph_utils.py
+++ project/graph_utils.py
@@ -1,8 +1,13 @@
+from collections import defaultdict, deque
+from itertools import product
 from typing import Tuple, NamedTuple, Set, Union, Iterable, Any
 
 import networkx.drawing.nx_pydot
 from networkx import MultiDiGraph
+from pyformlang.cfg import CFG, Terminal, Variable
 import cfpq_data
+
+from project.cfg_utils import to_weak_cfg, cfg_from_text, cfg_from_file
 
 
 class GraphInfo(NamedTuple):
@@ -95,3 +100,96 @@ def create_and_save_labeled_two_cycles_graph(
     """
     created_graph = create_labeled_two_cycles_graph(n, m, labels)
     save_graph(created_graph, path)
+
+
+def get_cnf(cfg: CFG) -> CFG:
+    weak_cfg = to_weak_cfg(cfg)
+    return weak_cfg.to_normal_form()
+
+
+def hellings_algorithm(cfg: CFG, graph: MultiDiGraph) -> Set[Tuple[str, str, str]]:
+    """
+    Computes the reachability information for all pairs of vertices in the given graph and context-free grammar.
+
+    Args:
+        cfg(CFG): The context-free grammar (CFG) to use for reachability analysis.
+        graph(MultiDiGraph): The directed graph on which to perform reachability analysis.
+
+    Returns:
+        Set[Tuple[int, Variable, int]]: A set of triples (start_vertex, nonterminal, end_vertex)
+        representing the reachability information for all pairs of vertices in the graph.
+    """
+    if graph.number_of_nodes() == 0:
+        return set()
+
+    wcnf = to_weak_cfg(cfg)
+    term_to_nonterm = defaultdict(set)
+    nonterm_pair_to_nonterm = defaultdict(set)
+    epsilon_nonterms = set()
+    for prod in wcnf.productions:
+        head, body = prod.head, prod.body
+        body_len = len(body)
+        if body_len == 0:
+            epsilon_nonterms.add(head)
+        elif body_len == 1:
+            term_to_nonterm[head].add(body[0])
+        elif body_len == 2:
+            nonterm_pair_to_nonterm[head].add((body[0], body[1]))
+
+    result = {(node, var, node) for node in graph.nodes for var in epsilon_nonterms}
+    result.update(
+        (i, n, j)
+        for i, j, label in graph.edges(data="label")
+        for n, terms in term_to_nonterm.items()
+        if Terminal(label) in terms
+    )
+
+    while True:
+        new_result = result.copy()
+        for i, j, k in product(graph.nodes, repeat=3):
+            for var1, pairs in nonterm_pair_to_nonterm.items():
+                for var2, var3 in pairs:
+                    if (i, var2, j) in result and (j, var3, k) in result:
+                        new = (i, var1, k)
+                        if new not in result:
+                            new_result.add(new)
+        if new_result == result:
+            break
+        result = new_result
+
+    return result
+
+
+def reachability_with_nonterminal(
+    grammar: CFG,
+    graph: MultiDiGraph,
+    start_vertices: Set,
+    end_vertices: Set,
+    target_nonterminal: Variable,
+) -> Set[Tuple[int, int]]:
+    """
+    Computes the reachability information for the specified start and end vertices
+    and the given nonterminal in the context-free grammar.
+
+    Args:
+        grammar(CFG): The context-free grammar (CFG) to use for reachability analysis.
+        graph(MultiDiGraph): The directed graph on which to perform reachability analysis.
+        start_vertices(Set[int]): A set of start vertices for which to compute reachability.
+        end_vertices(Set[int]): A set of end vertices for which to compute reachability.
+        target_nonterminal(Variable): The nonterminal to consider for reachability analysis.
+
+    Returns:
+        Set[Tuple[int, int]]: A set of pairs (start_vertex, end_vertex) representing the reachability
+        information for the specified start and end vertices and the given nonterminal in the CFG.
+    """
+    reachability = hellings_algorithm(grammar, graph)
+
+    filtered_reachability = {
+        (src, dest)
+        for src, nonterm, dest in reachability
+        if nonterm == target_nonterminal
+        and src in start_vertices
+        and dest in end_vertices
+    }
+
+    return filtered_reachability
---
+++ tests/test_hellings_algo.py
@@ -0,0 +1,89 @@
+import pytest
+from pyformlang.cfg import CFG, Variable
+from networkx import MultiDiGraph
+
+from project.graph_utils import hellings_algorithm, reachability_with_nonterminal
+from project.graph_utils import create_labeled_two_cycles_graph
+
+
+def test_hellings_algorithm():
+    cfg_text = "S -> a S b | eps"
+    cfg = CFG.from_text(cfg_text)
+    graph = MultiDiGraph()
+    graph.add_edges_from([(0, 1, {"label": "a"}), (1, 2, {"label": "b"})])
+
+    result = hellings_algorithm(cfg, graph)
+
+    expected_result = {(1, Variable("b#CNF#"), 2), (0, Variable("a#CNF#"), 1)}
+
+    assert result == expected_result
+
+
+def test_hellings_algorithm2():
+    cfg_text = """
+        S -> A B | B A
+        A -> a A b | a b
+        B -> b B a | b a
+    """
+    cfg = CFG.from_text(cfg_text)
+    graph = MultiDiGraph()
+    edges = [
+        (0, 1, {"label": "a"}),
+        (1, 2, {"label": "b"}),
+        (2, 3, {"label": "b"}),
+        (3, 4, {"label": "a"}),
+        (0, 5, {"label": "b"}),
+        (5, 6, {"label": "a"}),
+        (6, 7, {"label": "a"}),
+        (7, 8, {"label": "b"}),
+    ]
+    graph.add_edges_from(edges)
+
+    result = hellings_algorithm(cfg, graph)
+
+    expected_result = {
+        (0, Variable("C#CNF#1"), 3),
+        (0, Variable("A"), 2),
+        (2, Variable("B"), 4),
+        (6, Variable("A"), 8),
+        (3, Variable("a#CNF#"), 4),
+        (6, Variable("a#CNF#"), 7),
+        (0, Variable("b#CNF#"), 5),
+        (5, Variable("a#CNF#"), 6),
+        (1, Variable("b#CNF#"), 2),
+        (2, Variable("b#CNF#"), 3),
+        (0, Variable("S"), 4),
+        (0, Variable("S"), 8),
+        (0, Variable("a#CNF#"), 1),
+        (7, Variable("b#CNF#"), 8),
+        (0, Variable("C#CNF#2"), 7),
+        (0, Variable("B"), 6),
+    }
+    assert len(expected_result) == len(result)
+    for expected in expected_result:
+        assert expected in result
+
+
+def test_reachability_with_nonterminal():
+    cfg_text = """
+        S -> A B
+        S -> A S1
+        S1 -> S B
+        A -> a
+        B -> b
+        """
+    cfg = CFG.from_text(cfg_text)
+    graph = create_labeled_two_cycles_graph(2, 1, labels=("a", "b"))
+
+    start_vertices = graph.nodes
+    end_vertices = graph.nodes
+    target_nonterminal = Variable("S")
+
+    result = reachability_with_nonterminal(
+        cfg, graph, start_vertices, end_vertices, target_nonterminal
+    )
+
+    expected_result = {(0, 0), (0, 3), (1, 0), (1, 3), (2, 0), (2, 3)}
+    assert len(expected_result) == len(result)
+    for expected in expected_result:
+        assert expected in result
